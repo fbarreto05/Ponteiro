b13d4479f43ebad56fa933769edd9f3caa2686c2
entomo05 <fernandobarreto864@gmail.com>
2023-10-15 20:42:55 -0300

    Organização dos arquivos

--- ponteiroDjango/ponteiroDjango/Ponteiro/Tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class userAdmin(admin.ModelAdmin):
-    readonly_fields = ('login', 'password')
-
-# Register your models here.
